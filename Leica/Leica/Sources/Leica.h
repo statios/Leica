2104beb16edfad677b29a21fe51306a8a947b599
statios <statios@kakao.com>
2021-03-18 22:39:15 +0900

    Add project

---
+++ Leica/Leica/Sources/Leica.h
@@ -0,0 +1,18 @@
+//
+//  Leica.h
+//  Leica
+//
+//  Created by KIHYUN SO on 2021/03/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Leica.
+FOUNDATION_EXPORT double LeicaVersionNumber;
+
+//! Project version string for Leica.
+FOUNDATION_EXPORT const unsigned char LeicaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leica/PublicHeader.h>
+
+
