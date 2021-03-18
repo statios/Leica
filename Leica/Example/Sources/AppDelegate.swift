b5117f6b26b08f13ad3ff7aa4a6f1a8331a7bf0a
statios <statios@kakao.com>
2021-03-18 22:48:49 +0900

    Add example project

---
+++ Leica/Example/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by KIHYUN SO on 2021/03/18.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
---
+++ Leica/Example/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by KIHYUN SO on 2021/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
