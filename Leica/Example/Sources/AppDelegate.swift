a0476c8626876b3d91d6e0db00641d2b52b24903
statios <statios@kakao.com>
2021-03-20 02:18:08 +0900

    Update Example

--- Leica/Example/Sources/AppDelegate.swift
+++ Leica/Example/Sources/AppDelegate.swift
@@ -10,10 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  var window: UIWindow?
 
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
     return true
   }
 
--- Leica/Example/Sources/ViewController.swift
+++ Leica/Example/Sources/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import Leica
 
 class ViewController: UIViewController {
-
+  
+  let typingLabel = LeicaLabel()
+  let text = "동해물과 백두산이 마르고 닳도록\n"
+    + "하느님이 보우하사 우리 나라만세\n"
+    + "무궁화 삼천리 화려강산\n"
+    + "대한사람 대한으로 길이 보전하세"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .systemGreen
+    view.addSubview(typingLabel)
+    
+    typingLabel.translatesAutoresizingMaskIntoConstraints = false
+    typingLabel.leadingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+      constant: 32
+    ).isActive = true
+    typingLabel.trailingAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -32
+    ).isActive = true
+    typingLabel.topAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32
+    ).isActive = true
+    typingLabel.bottomAnchor.constraint(
+      equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -32
+    ).isActive = true
+    typingLabel.textColor = .white
+    typingLabel.font = UIFont.boldSystemFont(ofSize: 32)
   }
-
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    typingLabel.animate(text: text, style: .typing, loop: true)
+  }
 
 }
 
